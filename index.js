ee3138faba01b5a12f58c20617380c73a9b732cf
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-17 15:10:31 +0300

    added event to complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  /*function moveDodgerRight() {
+    const leftNumbers= dodger.style.left.replace("px", "");
+    const left= parseInt(lefttNumbers, 360);
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`;
+    
+    } 
+  }
+  document.addEventListener("keyDown", function (e) {
+    if(e.key==="arrowleft"){
+        moveDodgerRight();
+    }
+
+  });*/
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10) || 0;
+
+    if (left < 360) { // it's bound
+        dodger.style.left = `${left + 1}px`; // Move right
+    }
+}
